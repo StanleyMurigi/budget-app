8a68d8ee5be3b13f75dced0cde7087963e9f77b8
StanleyMurigi <stannjoroge643@gmail.com>
2025-03-09 20:07:42 +0300

    deployment config

--- budget_api/urls.py
+++ budget_api/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def home_redirect(request):
+    return redirect('/budget/')
 
 urlpatterns = [
+    path('', home_redirect),
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('budget/', include('budget.urls')),
