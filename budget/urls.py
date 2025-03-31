e421e62c4f2b867b51588f0c8d2aad6728b64259
Stanley Murigi <stannjoroge643@gmail.com>
2025-03-31 15:58:04 +0300

    urls configuration

--- budget/urls.py
+++ budget/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-from .views import CategoryListCreateView, BudgetListCreateView, BudgetDetailView, index, BudgetListView, BudgetCreateView, category_list, category_create
+from Api/views import CategoryListCreateView, BudgetListCreateView, BudgetDetailView, BudgetListView, BudgetCreateView
 from . import views
 
 urlpatterns = [
     path('', index, name='index'),
     path('manage/', views.manage_budgets, name='manage-budgets'),
-    path('budgets/', BudgetListCreateView.as_view(), name='budget-list-create'),
-    path('budgets/<int:pk>/', BudgetDetailView.as_view(), name='budget-detail'),
-    path("list/", BudgetListView.as_view(), name="budget-list"),
-    path("create/", BudgetCreateView.as_view(), name="budget-create"),
     path("categories/", category_list, name="category_list"),
     path("categories/<int:budget_id>/", category_list, name="category_list"),
     path("categories/add/<int:budget_id>/", category_create, name="category-create"),
@@ -17,5 +13,12 @@ urlpatterns = [
     path("usage/<int:category_id>/", views.usage_list, name="usage-list"),
     path("usage/add/<int:category_id>/", views.usage_create, name="usage-create"),
     path("usage/", views.usage_overview, name="usage-overview"),  # View all categories + usage
+
+
+    #api urls
+    path('budgets/', BudgetListCreateView.as_view(), name='budget-list-create'),
+    path('budgets/<int:pk>/', BudgetDetailView.as_view(), name='budget-detail'),
+    path("list/", BudgetListView.as_view(), name="budget-list"),
+    path("create/", BudgetCreateView.as_view(), name="budget-create"),
 ]
 
